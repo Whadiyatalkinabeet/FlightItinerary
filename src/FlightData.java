96b5033f57cbfe6e93c2dd44f504fe39a279de55
Whadiyatalkinabeet <NyalSadiq@hotmail.com>
2016-11-22 02:13:59 +0000

    Finish TimeBuilder

--- src/FlightData.java
+++ src/FlightData.java
@@ -4,18 +4,22 @@ public class FlightData extends org.jgrapht.graph.DefaultWeightedEdge {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private String Number, Depart, Arrive;
+	private String Number, Depart, Arrive, time;
 	private Integer Price, Length;
 	
 
-	public FlightData (Integer Price, String Number, String Depart, String Arrive, Integer Length){
+	public FlightData (Integer Price, String Number, String Depart, String Arrive, Integer Length, String time){
 		this.Price = Price;
 		this.Number = Number;
 		this.Depart = Depart;
 		this.Arrive = Arrive;
 		this.Length = Length;
+		this.time = time;
 		
-		
+	}
+	
+	public String getTime() {
+		return time;
 	}
 	
 	public Integer getPrice(){
